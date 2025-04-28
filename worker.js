fca3fe6c019c4fffb6a1610b89b5958c92c7f013
ryzXD. <111454488+ryz-code@users.noreply.github.com>
2025-04-28 23:49:56 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,11 @@
 // ---------- Insert Your Data ---------- //
 
-const BOT_TOKEN = "BOT_TOKEN"; // Insert your bot token.
+const BOT_TOKEN = "7741593684:AAHSnyirgeIBDyvMBynhSpgnTgVAMAFHo2U"; // Insert your bot token.
 const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
-const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
-const BOT_OWNER = 123456789; // Insert your telegram account id.
-const BOT_CHANNEL = -100123456789; // Insert your telegram channel id which the bot is admin in.
-const SIA_SECRET = "SIA_SECRET"; // Insert a powerful secret text and keep it safe.
+const BOT_SECRET = "Persija"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
+const BOT_OWNER = 7973732709; // Insert your telegram account id.
+const BOT_CHANNEL = 7973732709; // Insert your telegram channel id which the bot is admin in.
+const SIA_SECRET = "ryBL52xFqWYffPKK446k"; // Insert a powerful secret text and keep it safe.
 const PUBLIC_BOT = false; // Make your bot public (only [true, false] are allowed).
 
 // ---------- Do Not Modify ---------- // 
